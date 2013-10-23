bb6773d9b33c572b99c04041e46bf1e9ac0ddbf0
Brandon Weaver <keystonelemur@gmail.com>
2013-10-23 16:21:47 -0500

    Update handyman.rb

--- lib/handyman.rb
+++ lib/handyman.rb
@@ -23,4 +23,55 @@ module Handyman
     end
 
   end
+  
+    class Blueprint
+    attr_accessor :ssh, :run_list, :config_file, :opts
+
+    def initialize(host, user, password)
+      @ssh = Net::SSH.start(host, user, password: password)
+      @run_list = []
+      @config_file = 'lib/config.yml'
+      @opts = {
+        name: host,
+        dir: '/var/www',
+        current_branch: 'master'
+      }
+    end
+
+    def pretty_runlist
+      @run_list.each do |instruction|
+        instruction.each_pair do |title, commands|
+          puts title
+          commands.to_a.each{ |command| puts "  #{command}" }
+          puts
+        end
+      end
+    end
+
+    def build
+      configuration_instructions.each do |instruction|
+        @run_list << { instruction[0] => instruction[1] }
+      end
+    end
+
+    def configuration_instructions
+      YAML.load interpolated_config
+    end
+
+    def interpolated_config
+       File.open(@config_file,'r').each_line.reduce(''){ |str, line|
+         str << line % opts
+       }
+    end
+
+    def execute
+      @run_list.each do |instruction|
+        instruction.each_pair do |title, commands|
+          puts title
+          commands.to_a.each{ |command| ssh.exec! command }
+        end
+      end
+    end
+
+  end
 end
