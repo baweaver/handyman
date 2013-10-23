d9ec3318229a004bf53782ea4385470567e9d3c9
Brandon Weaver <keystonelemur@gmail.com>
2013-10-23 17:06:44 -0500

    Update handyman.rb

--- lib/handyman.rb
+++ lib/handyman.rb
@@ -6,48 +6,51 @@ module Handyman
   class << self
 
     # Build the instruction set to execute on the remote box
-    def build(blueprint, branch='default')
-      # ...
+    def make(blueprint, host, user, password)
+      blueprint = Blueprint.new(host, user, password)
+      blueprint.build
+      blueprint.execute
     end
 
   end
 
   class Blueprint
-  attr_accessor :ssh, :run_list, :config_file, :opts
+    attr_accessor :ssh, :run_list, :config_file, :opts
 
-  def initialize(host, user, password)
-    @ssh = Net::SSH.start(host, user, password: password)
-    @run_list = []
-    @config_file = 'lib/config.yml'
-    @opts = {
-      email: 'test@test.com'
-    }
-  end
-
-  def build
-    configuration_instructions.each do |instruction|
-      @run_list << { instruction[0] => instruction[1] }
+    def initialize(host, user, password)
+      @ssh = Net::SSH.start(host, user, password: password)
+      @run_list = []
+      @config_file = 'lib/config.yml'
+      @opts = {
+        email: 'test@test.com'
+        name: 'John Doe'
+      }
     end
-  end
-
-  def configuration_instructions
-    YAML.load interpolated_config
-  end
-
-  def interpolated_config
-     File.open(@config_file,'r').each_line.reduce(''){ |str, line|
-       str << line % opts
-     }
-  end
-
-  def execute
-    @run_list.each do |instruction|
-      instruction.each_pair do |title, commands|
-        puts title
-        commands.to_a.each{ |command| ssh.exec! command }
+  
+    def build
+      configuration_instructions.each do |instruction|
+        @run_list << { instruction[0] => instruction[1] }
       end
     end
-  end
+  
+    def configuration_instructions
+      YAML.load interpolated_config
+    end
+  
+    def interpolated_config
+       File.open(@config_file,'r').each_line.reduce(''){ |str, line|
+         str << line % opts
+       }
+    end
+  
+    def execute
+      @run_list.each do |instruction|
+        instruction.each_pair do |title, commands|
+          puts title
+          commands.to_a.each{ |command| ssh.exec! command }
+        end
+      end
+    end
 
   end
 end
